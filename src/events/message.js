2df06c8ef2a4e570e1c1e7011507cf5857a98a31
SayanSahu99 <sayansovan@gmail.com>
2021-04-02 12:41:03 +0530

    Discord Events

---
+++ src/events/message.js
@@ -0,0 +1,22 @@
+const play = require("../commands/play");
+const { PREFIX } = require("../util/utils");
+
+async function messageEvent(message, command, queue) {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+  
+    if (!message.content.startsWith(PREFIX)) return;
+  
+    const serverQueue = queue.get(message.guild.id); 
+  
+    if (message.content.startsWith(`${PREFIX}play`)) {
+      command.get("play").play(message, serverQueue, queue);
+    }
+  
+}
+
+module.exports = {
+    name: "message",
+    messageEvent: messageEvent
+}
