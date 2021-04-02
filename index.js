c5cf49333edc599c3fa4a19650fe2fd533550c03
SayanSahu99 <sayansovan@gmail.com>
2021-04-02 12:38:52 +0530

    Reading the events dir to get events

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const { readdirSync } = require("fs");
 const { join } = require("path");
 const bot = require("./src/bot");
-const { PREFIX } = require("./src/util/utils");
+const { Collection } = require("discord.js");
 
+const queue = new Map();
+let queueContruct = {};
+let command, event;
+bot.commands = new Collection();
+bot.events = new Collection();
 
 /** Reading the commands dir to get the commands**/
 const commandFiles = readdirSync(join(__dirname, "src/commands")).filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
   command = require(join(__dirname, "src/commands", `${file}`));
+  bot.commands.set(command.name, command);
 }
+
+/** Reading the events dir to get the events**/
+const eventFiles = readdirSync(join(__dirname, "src/events")).filter(file => file.endsWith(".js"));
+for(const file of eventFiles) {
+  event = require(join(__dirname, "src/events", `${file}`));
+  bot.events.set(event.name, event);
+}
+
+bot.on('message', (message) => bot.events.get("message").messageEvent(message, bot.commands, queue));
+
 
 process.on('unhandledRejection', err => console.log(err));
