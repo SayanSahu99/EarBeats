fa96e202794bfd4bcdaa268c3c5ffecaa05b1d01
SayanSahu99 <sayansovan@gmail.com>
2021-04-03 16:29:11 +0530

    Stop Command

---
+++ src/commands/stop.js
@@ -0,0 +1,20 @@
+
+function stop(message, serverQueue) {
+    console.log(serverQueue);
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+  
+    if (!serverQueue)
+      return message.channel.send("There is no song that I could stop!");
+  
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+  
+}
+
+module.exports = {
+    name: "stop",
+    stop: stop
+}
